e4127099a287ea15c5a82a229f4798d8a3241b25
gsarig <contact@gsarigiannidis.gr>
2022-11-20 02:33:49 +0200

    Updates Leaflet to v.1.9.3

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cf752fe6619bc00539ad');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5e70b92b65fae801c3b6');

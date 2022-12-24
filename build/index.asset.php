9eb388017eb2dde7f80e82e5edd707f72636d86c
gsarig <contact@gsarigiannidis.gr>
2022-12-25 01:03:30 +0200

    Updates `react-leaflet` to v.4.2.0

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3388e1e60c9f4c41dec9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8ecc5c41e5f4618ef00d');

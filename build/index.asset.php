73a28797acd4b57b0baf1498e7add5f2ef1ad2c1
gsarig <contact@gsarigiannidis.gr>
2022-12-25 18:38:18 +0200

    Adds polygon support

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3388e1e60c9f4c41dec9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a98f5403eaf05d12a8c1');

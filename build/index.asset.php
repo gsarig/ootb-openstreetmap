5f5c4782359894897271f724fe5892c03e8cd49e
gsarig <contact@gsarigiannidis.gr>
2023-03-05 01:58:37 +0200

    Adds support for separate icon per marker

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b02df17e5e9500f32cdc');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '702b7e07e7b4be9d1a63');
---
+++ src/Helpers/getFallbackIcon.js
@@ -0,0 +1,8 @@
+export default function getFallbackIcon() {
+	//noinspection JSUnresolvedVariable
+	const {
+		pluginDirUrl,
+	} = ootbGlobal;
+
+	return pluginDirUrl + 'assets/vendor/leaflet/images/marker-icon.png';
+}

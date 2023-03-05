ba7d435ed64ef995c13370b957870866129b6204
gsarig <contact@gsarigiannidis.gr>
2023-03-05 17:17:10 +0200

    Update build scripts

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '675b7993527eb88a189f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '81d21f183f471bced303');

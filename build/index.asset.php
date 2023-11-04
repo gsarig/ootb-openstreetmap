c4d94707945610c0b56022e326d02bffb0c3bad0
gsarig <contact@gsarigiannidis.gr>
2023-11-04 20:07:02 +0200

    Fixes a bug with the delete button @link: https://wordpress.org/support/topic/when-a-marker-is-deleted-the-wrong-one-is-deleted/

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1c9a3e27205b0c8e9d88');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0a3f43692b7c0a063a66');

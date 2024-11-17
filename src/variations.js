509256ac21092077afe700cdf9b2c384952c9454
gsarig <contact@gsarigiannidis.gr>
2024-11-18 00:48:45 +0200

    Updates the build scripts

--- src/variations.js
+++ src/variations.js
@@ -1,6 +1,8 @@
 // noinspection NpmUsedModulesInstalled,JSUnresolvedVariable
 
+wp.domReady(() => {
 // Unregister the `custom-fields` variation if the Location setting is disabled.
-if (!ootbGlobal?.options?.geodata) {
-	wp.blocks.unregisterBlockVariation('ootb/openstreetmap', 'custom-fields');
-}
+	if (!ootbGlobal?.options?.geodata) {
+		wp.blocks.unregisterBlockVariation('ootb/openstreetmap', 'custom-fields');
+	}
+});

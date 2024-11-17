95f31213e469312be9d67f17ac1be05de5a40e54
gsarig <contact@gsarigiannidis.gr>
2024-11-18 00:30:34 +0200

    Adds build assets

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'fa8efa31d5326e923715');

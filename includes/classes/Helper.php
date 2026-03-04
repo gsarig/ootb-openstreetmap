f69d79bb48368c22f3f235af68fda6cbb32eb39a
mgiannopoulos24 <79588074+mgiannopoulos24@users.noreply.github.com>
2026-03-04 20:57:34 +0200

    Modify according to codesniffer rules

--- includes/classes/Helper.php
+++ includes/classes/Helper.php
@@ -409,16 +409,13 @@ class Helper {
 				'height' => ( preg_match( '/^\d+px$/', $value ) === 1 ) ? $value : self::get_default( 'height' ),
 				'provider' => in_array( $value, array_keys( self::providers( [ 'associative' => true ] ) ), true ) ? $value : '',
 				'maptype' => in_array( $value, self::map_types(), true ) ? $value : '',
-				'touchzoom', 'scrollwheelzoom', 'dragging', 'doubleclickzoom', 'fullscreen' => in_array( $value, [
-					'true',
-					'false'
-				], true ) ? $value : '',
-				'touchzoom', 'scrollwheelzoom', 'dragging', 'doubleclickzoom' => in_array(
+				'touchzoom', 'scrollwheelzoom', 'dragging', 'doubleclickzoom', 'fullscreen' => in_array(
 					$value,
 					[
 						'true',
 						'false',
-					], true
+					],
+					true
 				) ? $value : '',
 				'marker' => ( filter_var( $value, FILTER_VALIDATE_URL ) !== false ) ? $value : '',
 				default => $value,
